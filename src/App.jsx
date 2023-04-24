aeaaa5e084086bd8f2e52f9681eb2bedc59d4c8d
LucasEzequielSilva <91817152+LucasEzequielSilva@users.noreply.github.com>
2023-04-24 21:52:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, Fragment } from 'react'
+import './index.css'
+import Carousel from './Carousel'
+import Home from './Home'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <>
+      <Home/>
+      </>
+  )
+}
+
+export default App
